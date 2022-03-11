c1922bef54b347d52492329c4e7d95553dc1564e
donellgill <donell.gill@outlook.com>
2022-03-11 14:38:43 -0500

    initial commit

---
+++ src/components/Clock.js
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from "react"
+
+export default function Clock(){
+
+    const [cTime, setTime] = useState(new Date().toLocaleString())
+
+    useEffect(() => {
+        setInterval(() =>{
+            setTime(new Date().toLocaleString())
+        }, 1000);
+    })
+
+    return(
+        <div>
+            <h1>{cTime}</h1>
+
+            <p className="App-clock">
+                <button onClick = {() =>setTime(cTime)}>{cTime}</button>
+            </p>
+        </div>
+    )
+
+}
